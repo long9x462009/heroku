d30e0d49b7e76bb099320a50ad7c5e3cf14991b3
Nguyễn Long <mistergentle@Nguyens-MacBook-Pro.local>
2021-03-18 20:06:15 +0700

    replace project

--- src/main/java/com/example/heroku/Controller.java
+++ src/main/java/com/example/heroku/Controller.java
@@ -1,12 +1,14 @@
+package com.example.heroku;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/hello")
+    @GetMapping("/heroku")
     public String heroku(){
-        return "hello, from Heroku";
+        return "hello, heroku";
     }
 
 }
--- src/test/java/com/example/heroku/HerokuApplicationTests.java
+++ src/test/java/com/example/heroku/HerokuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.heroku;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HerokuApplicationTests {
 
 	@Test
 	void contextLoads() {
